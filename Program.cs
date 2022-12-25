7db5850cb83e795fdc54435e38bbed3b89fdac1a
ljlima <limajeanlucas@gmail.com>
2022-12-25 03:13:24 -0400

    Atribuicao variaveis

--- Program.cs
+++ Program.cs
@@ -4,6 +4,12 @@ class Programa
 {
     static void Main(String[] args){
         Console.WriteLine("Hello, World!\n");
+        int nota1, nota2, soma;
+
+        nota1 = 2;
+        nota2 = 3;
+        soma = nota1 + nota2;
+        Console.WriteLine("A soma total das notas é: " + soma   + "\n" + "Execução em :"  + DateTime.Now);
         Console.WriteLine("pressione Enter....\n");
         Console.ReadLine();
     }
