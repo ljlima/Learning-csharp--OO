2157a52c69b992bd65f3cde46cb70c250fa770b2
ljlima <limajeanlucas@gmail.com>
2022-12-25 04:50:20 -0400

    uso de ponto flutuante longo

--- Program.cs
+++ Program.cs
@@ -4,10 +4,10 @@ class Programa
 {
     static void Main(String[] args){
         Console.WriteLine("Hello, World!\n");
-        int nota1, nota2, soma;
+        double nota1, nota2, soma;
 
-        nota1 = 2;
-        nota2 = 3;
+        nota1 = 2.65;
+        nota2 = 3.09;
         soma = nota1 + nota2;
         Console.WriteLine("A soma total das notas é: " + soma   + "\n" + "Execução em :"  + DateTime.Now);
         Console.WriteLine("pressione Enter....\n");
