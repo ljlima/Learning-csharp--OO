882c1b54c9d5677f62e7d5343775da4bd52198a3
ljlima <limajeanlucas@gmail.com>
2022-12-26 23:55:13 -0400

    projetoPiramide

---
+++ projeto_piramide_numerica/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Programa{
+    static void Main(String[] args){
+    int contadorColuna, contadorLinha;
+    for(contadorLinha=1;contadorLinha<=5;contadorLinha++){
+        Console.WriteLine();
+        for(contadorColuna=1;contadorColuna<=contadorLinha;contadorColuna++){
+            Console.Write(contadorColuna);
+        }
+    }
+}
+}
